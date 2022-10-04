325d7e71a986b04bb548f489ea8c40d2f78f3c10
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-10-04 07:58:07 +0300

    frontend: finish register client

--- frontend/js/login_script.js
+++ frontend/js/login_script.js
@@ -1,5 +1,6 @@
 //Initialize APIS
 const loginAPI = "http://127.0.0.1:8001/api/login";
+const registerAPI = "http://127.0.0.1:8001/api/register";
 
 //Initialize variables
 const openSignin = document.getElementById("open_signin");
@@ -12,13 +13,14 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const error = document.getElementById("error");
 
-const new_email = document.getElementById("new_email");
-const new_username = document.getElementById("new_username");
-const new_password = document.getElementById("new_password");
-const new_firstName = document.getElementById("new_first_name");
-const new_lastName = document.getElementById("new_last_name");
-const new_address = document.getElementById("new_address");
-const new_phoneNumber = document.getElementById("new_phone_number");
+const registerButton = document.getElementById("create_account");
+const locationButton = document.getElementById("location_btn");
+const newName = document.getElementById("new_name");
+const newEmail = document.getElementById("new_email");
+const newPassword = document.getElementById("new_password");
+const newLocation = document.getElementById("new_address");
+const gender_id = 3;
+const x = document.getElementById("demo");
 
 //On click, show signup block
 openSignin.onclick = () =>{
@@ -61,14 +63,35 @@ const login = () => {
 
 const createNewAccount = () => {
     //Save user's data
+    const chosenYear = year.options[year.selectedIndex];
+    const currentYear = new Date().getFullYear() ;
+    const age = currentYear - chosenYear.value;
+    const gender = document.querySelector('input[name="gender"]:checked').value;
+
+    var checkedValue = []; 
+    var inputElements = document.getElementsByClassName('interest');
+    for(var i=0; i < inputElements.length; i++){
+          if(inputElements[i].checked){
+               checkedValue.push(inputElements[i].value);
+          }
+    }
+
+    if(checkedValue.length == 1 && checkedValue[0] == "Female"){
+        gender_id = 1
+    }
+    else if(checkedValue.length == 1 && checkedValue[0] == "Male"){
+        gender_id = 2
+    }
+
     const data = new FormData();
-    data.append("email", new_email.value);
-    data.append("username", new_username.value);
-    data.append("password", new_password.value);
-    data.append("firstName", new_firstName.value);
-    data.append("lastName", new_lastName.value);
-    data.append("address", new_address.value);
-    data.append("phoneNumber", new_phoneNumber.value);
+    data.append("name", newName.value);
+    data.append("email", newEmail.value);
+    data.append("password", newPassword.value);
+    data.append("age", age);
+    data.append("gender", gender);
+    data.append("gender_id", gender_id)
+    data.append("longitude", longitude);
+    data.append("latitude", latitude);
 
     //Send data to the server using axios
     axios.post(signupAPI, data)
@@ -83,7 +106,24 @@ const createNewAccount = () => {
         window.location.replace("landingPage.html");
     })
     .catch((e)=>
-     errorCreate.textContent = "Invalid input");
+        errorCreate.textContent = "Invalid input");
+}
+
+async function getLocation() {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(position => {
+        resolve(showPosition(position)); 
+      } , reject);
+    });
+}
+  
+function showPosition(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude
+    x.innerHTML = "Latitude: " + latitude + 
+    "<br>Longitude: " + longitude;
 }
 
 loginButton.addEventListener("click", login);
+registerButton.addEventListener("click", createNewAccount);
+locationButton.addEventListener("click", getLocation())
