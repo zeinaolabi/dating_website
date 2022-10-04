90d6fdf4daae540573cf26a618a4b97b1598f2db
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-10-04 09:40:03 +0300

    frontend: finish change profile picture client

--- frontend/js/modals.js
+++ frontend/js/modals.js
@@ -1,4 +1,9 @@
+//Initialize APIs
+const updateProfileAPI = "http://127.0.0.1:8001/api/auth/update_profile";
+
+
 //Initialize variables
+const userID = localStorage.getItem("userID");
 const openEditModal = document.getElementById("open_edit_modal");
 const openMatchModal = document.getElementById("open_match_modal");
 const openHomeBtn = document.getElementById("open_home");
@@ -8,6 +13,13 @@ const editModal = document.getElementById("edit_profile_modal");
 const matchModal = document.getElementById("match_modal");
 const close = document.getElementById("close");
 const close2 = document.getElementById("close2");
+
+const addedImage = document.getElementById("added_image");
+const config = {
+    headers: {
+      Authorization: localStorage.getItem("token")
+    }
+}
 
 // When the user clicks on the button, open the modal
 openEditModal.onclick = function() {
@@ -49,3 +61,44 @@ window.onclick = function(event) {
         matchModal.style.display = "none";
     }
 }
+
+addedImage.addEventListener('change',()=>{
+    //Save added image value and files
+    const addedImageValue = addedImage.value;
+    const [file] = addedImage.files;
+    
+    //If picture is empty, do nothing
+    if(addedImageValue == ""){
+        return
+    }
+        
+    const updateProfilePicture = (base64Image) => {
+        const data = new FormData();
+        data.append("id", userID);
+        data.append("image", base64Image);
+    
+        //Send data to the server using axios
+        axios.post(updateProfileAPI, data, config)
+        .then(
+            response =>  {
+                let image = document.querySelector(".profile_picture");
+    
+                if(file){
+                    image.src = base64Image;
+                }
+        })
+        .catch((e)=>{
+            console.log(e);
+        })
+    }
+    
+    const reader = new FileReader();
+    reader.addEventListener("load", (event) => {
+        // convert image file to base64 string
+        updateProfilePicture(event.currentTarget.result)
+    }, false);
+    
+    if(file){
+        reader.readAsDataURL(file);
+    }
+})
